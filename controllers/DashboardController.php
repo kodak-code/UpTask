1d13851affc8b9486d66ca88fb3080d889dde88b
kodak-code <matiasmamani163@gmail.com>
2025-02-18 15:25:24 -0300

    creando proyectos y vista

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,31 +2,89 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
 use MVC\Router;
 
-class DashboardController {
-    public static function index(Router $router) {
+class DashboardController
+{
+    public static function index(Router $router)
+    {
 
         session_start(); // Arrancar la sesion del anterior controller
 
         isAuth();
 
-        // parte de proyectos, logica de negocio de la App
+        $id = $_SESSION['id'];
 
+        $proyectos = Proyecto::belongsto('propietarioId', $id);
+        
         $router->render('dashboard/index', [
-            'titulo' => 'Proyectos'
+            'titulo' => 'Proyectos',
+            'proyectos' => $proyectos
         ]);
     }
-    public static function crear_proyecto(Router $router) {
+    public static function crear_proyecto(Router $router)
+    {
 
-        session_start(); 
+        session_start();
+
+        isAuth();
+
+        $alertas = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $proyecto = new Proyecto($_POST);
+
+            // Validacion
+            $alertas = $proyecto->validarProyecto();
+
+            if (empty($alertas)) {
+                // Generar una url unica
+                $hash = md5(uniqid());
+                $proyecto->url = $hash;
+
+                // Almacenar el creador del proyecto
+                $proyecto->propietarioId = $_SESSION['id'];
+
+                // Guardar el proyecto
+                $proyecto->guardar();
+
+                // Redireccionar
+                header('Location: /proyecto?id=' . $proyecto->url);
+            }
+        }
+
         $router->render('dashboard/crear-proyecto', [
-            'titulo' => 'Crear Proyecto'
+            'titulo' => 'Crear Proyecto',
+            'alertas' => $alertas
         ]);
     }
-    public static function perfil(Router $router) {
+    public static function proyecto(Router $router)
+    {
 
-        session_start(); 
+        session_start();
+
+        isAuth();
+
+        $token = $_GET['id']; // la url
+
+        if (!$token) header('Location: dashboard');
+
+        // Revisar que la persona que visita el proyecto es quien lo creo
+        $proyecto = Proyecto::where('url', $token);
+
+        if ($proyecto->propietarioId !== $_SESSION['id']) header('Location: dashboard');
+
+        $router->render('dashboard/proyecto', [
+            'titulo' => $proyecto->proyecto
+        ]);
+    }
+    public static function perfil(Router $router)
+    {
+
+        session_start();
+
         $router->render('dashboard/perfil', [
             'titulo' => 'Perfil'
         ]);
